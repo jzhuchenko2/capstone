3cd3dfb1d2b9461a9c3fad0f20ba190bffddfe7e
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-08-17 20:01:18 -0500

    point of the 3dimensional structure

--- main.cpp
+++ main.cpp
@@ -7,3 +7,8 @@
 // constants
 const double G = 6.674e-11;    // gravitational constant
 const double c = 299792458;     // speed of light
+
+// structure to represent a 3D point
+struct Point {
+    double x, y, z;
+};
