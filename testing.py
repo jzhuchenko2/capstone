c86ac3d70b09ee6185c66530c0f4f5b7bbebeeb4
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-10-13 15:51:04 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -32,3 +32,5 @@ def update(frame):
   theta2 = theta_vals[frame] #always two thetas and adjust if needed for different motion
   black_hole1_x = L4_x + R_s1 * np.cos(theta2)
   black_hole1_y = L4_y + R_s1 * np.sin(theta2)
+
+  black_hole2_x = L5_x + R_s1 * np.cos(theta2)
